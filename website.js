6f2641f4b043dbef01a523f1a2f2048addf64e07
jordangottlieb99 <jordangottlieb99@gmail.com>
2016-08-02 10:15:52 -0700

    Update website.js

--- website.js
+++ website.js
@@ -1,3 +1,28 @@
+if (window.openDatabase) {
+
+    var mydb = openDatabase("clubs_db", "0.1", "Database of Clubs Registered", 1024 * 1024);
+   
+    mydb.transaction(function(t) {
+        t.executeSql("CREATE TABLE IF NOT EXISTS clubsdata (email TEXT PRIMARY KEY, firstname TEXT, lastname TEXT, clubname TEXT, purpose TEXT, events TEXT, fundraisers TEXT, category INTEGER, image TEXT)"); //creates table 
+	t.executeSql("CREATE TABLE IF NOT EXISTS users (email TEXT PRIMARY KEY, firstname TEXT, lastname TEXT, password TEXT)"); //creates table 
+
+    });
+} else {
+    alert("WebSQL is not supported by your browser!"); 
+}
+
+function category1 (){
+	if (mydb) {
+		mydb.transaction(function(t){
+		var dataimg = t.executeSql("SELECT image FROM clubsdata WHERE categories = 1");
+		var src = document.getElementById("imgg").src
+		src.innerHTML = dataimg
+		})
+	else 
+		alert("db not found!")
+	}	
+}
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";  
