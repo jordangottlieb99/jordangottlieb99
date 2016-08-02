12a86d63d4b5f33d43063b95be735ce17f5d9d78
jordangottlieb99 <jordangottlieb99@gmail.com>
2016-08-02 10:01:54 -0700

    Add files via upload

---
+++ website.js
@@ -0,0 +1,41 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";  
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+function Academics() {
+    var catt1 = ["Academic Clubs", ""];
+    document.getElementById("test").innerHTML = catt1[0];
+    document.getElementById("test2").innerHTML = catt1[1];
+	document.getElementById("thumbnail").className= "unhidden"
+	
+}
+function Service() {
+    var catt1 = ["Service Clubs", ""];
+    document.getElementById("test").innerHTML = catt1[0];
+    document.getElementById("test2").innerHTML = catt1[1];
+}
+function Other() {
+    var catt1 = ["Service Clubs", ""];
+    document.getElementById("test").innerHTML = catt1[0];
+    document.getElementById("test2").innerHTML = catt1[1];
+}
+function SGA() {
+    var catt1 = ["Service Clubs", ""];
+    document.getElementById("test").innerHTML = catt1[0];
+    document.getElementById("test2").innerHTML = catt1[1];
+}
+function Social() {
+    var catt1 = ["Service Clubs", ""];
+    document.getElementById("test").innerHTML = catt1[0];
+    document.getElementById("test2").innerHTML = catt1[1];
+}
+function Honor() {
+    var catt1 = ["Service Clubs", ""];
+    document.getElementById("test").innerHTML = catt1[0];
+    document.getElementById("test2").innerHTML = catt1[1];
+}
